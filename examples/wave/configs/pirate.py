db5f66044ffd506be31b5f3f2e63dddf14e47b68
Junmiao <junmiao.hu1@gmail.com>
2025-08-12 16:11:21 -0700

    added pirate naming

--- examples/wave/configs/pirate.py
+++ examples/wave/configs/pirate.py
@@ -12,7 +12,7 @@ def get_config():
     # Weights & Biases
     config.wandb = wandb = ml_collections.ConfigDict()
     wandb.project = "PINN-Wave"
-    wandb.name = "pirate"
+    wandb.name = "pirate_fp64"
     wandb.tag = None
 
     # Physics-informed initialization
